f557f9236e50376007ad68b3fca0476be4a30d1f
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-06-20 14:18:11 -0300

    feat: Create route.ts file for managing application routes

---
+++ app/api/routes.ts
@@ -0,0 +1,7 @@
+export const routes = [
+  { name: 'HOME', route: '/' },
+  { name: 'SOBRE', route: '/about' },
+  { name: 'SERVIÇOS', route: '/services' },
+  { name: 'FOTOS', route: '/gallery' },
+  { name: 'AGENDAMENTO', route: '/contact' },
+]
