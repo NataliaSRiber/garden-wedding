349c94a36d1b02dd5c8a166a0c519e7eaa6c75e6
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-06-20 14:17:08 -0300

    feat: Add new colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,27 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      spacing: {
+        '112': '48rem',
+      },
+      colors: {
+        green1: {
+          500: '#A3B18A',
+        },
+        green2: {
+          600: '#588157',
+        },
+        green3: {
+          700: '#3A5A40',
+        },
+
+        green4: {
+          800: '#344E41',
+        },
+        gray1: {
+          800: '#DAD7CD',
+        },
+       },
     },
   },
   plugins: [],
